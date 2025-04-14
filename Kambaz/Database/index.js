0e30198e127d639e152afb17549c90eaaca70cd8
Iris Carrillo <carrillo.iris2020@gmail.com>
2025-04-13 21:46:51 -0400

    Update server code for final project

--- Kambaz/Database/index.js
+++ Kambaz/Database/index.js
@@ -3,11 +3,13 @@ import courses from './courses.js';
 import modules from './modules.js';
 import users from './users.js';
 import enrollments from './enrollments.js';
+import quizzes from "./quizzes.js";
 
 export default {
   assignments,
   courses,
   modules,
   users,
-  enrollments
+  enrollments,
+  quizzes
 };
