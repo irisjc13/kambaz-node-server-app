ae15c8b3ec0cd86f354608046d46cf6615189883
Iris Carrillo <carrillo.iris2020@gmail.com>
2025-04-16 14:16:35 -0400

    index.js a6

--- index.js
+++ index.js
@@ -8,30 +8,21 @@ import "dotenv/config";
 import session from "express-session";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
-import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
-mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "https://a6--kambaz-react-web-app-sp25.netlify.app",
-      ];
-
-      const isNetlify = origin?.endsWith(".netlify.app");
-      const isLocalhost = allowedOrigins.includes(origin);
-
-      if (!origin || isNetlify || isLocalhost) {
+    origin: function (origin, callback) {
+      if (
+        !origin ||
+        origin.endsWith(".netlify.app") ||
+        origin === "http://localhost:5173"
+      ) {
         callback(null, true);
       } else {
-        console.log("Blocked origin:", origin);
         callback(new Error("Not allowed by CORS"));
       }
     },
